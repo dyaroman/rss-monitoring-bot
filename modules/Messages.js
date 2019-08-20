7d35b7609bffc5dd9c32241cf0c20b2b26f8ae66
Roman Diachenko <romand@leadsmarket.com>
2019-08-20 10:43:11 +0300

    refactoring

--- modules/Messages.js
+++ modules/Messages.js
@@ -1,6 +1,6 @@
 const Messages = {
-  startMessage: `
-Hello there, I am MonitoringBot!
+    startMessage: `
+Hello there, I am MonitoringBot 👀!
 Let's start, please send me "/monitoring" or choose it in commands.
   `
 };

1cd0c73fa68a40ba60bed49b78fa9ccb3b078c7b
Roman Diachenko <romand@leadsmarket.com>
2019-08-20 11:38:16 +0300

    refactoring messages text

--- modules/Messages.js
+++ modules/Messages.js
@@ -3,17 +3,22 @@ const Messages = {
 Hello there, I am RuTrackerRssMonitoringBot 👀!
 Let's start, please send me "/monitoring" or choose it in commands.
   `,
-    controlsButtons: `Choose your command below:`,
+
+    controlsButtons: `Choose command below:`,
     addNewMonitoringButton: `✅ Add new monitoring`,
     removeMonitoringButton: `❌ Remove monitoring`,
     removeAllMonitoringsButton: `🧨 Remove all monitorings`,
-    showMonitoringsButton: `👀 Show monitorings`,
+    showMonitoringsButton: `👀 Show active monitorings`,
     runSearchButton: `🚀 Run search`,
+
     noSearchResult: `❎ No result`,
-    addNewMonitoringQuestion: `What do want to monitor?`,
-    removeMonitoringQuestion: `What monitoring do want to remove?`,
-    noActiveMonitorings: `❎ No active monitorings`,
+    noActiveMonitorings: `❎ No active monitorings!`,
+
+    addNewMonitoringQuestion: `What movie or TV show do want to monitoring?`,
+    removeMonitoringQuestion: `What monitoring do you want to remove?`,
+
     allMonitoringsRemoved: `✅ All monitorings removed!`,
+
     addedNewMonitoring: `monitoring added!`,
     removedMonitoring: `monitoring removed!`,
 };

33f4fdb521e6c6f9ede1cc30447fa9f2785c8aed
Roman Diachenko <dyaroman@gmail.com>
2019-08-21 09:14:03 +0300

    refactoring commands naming

---
+++ modules/Commands.js
@@ -0,0 +1,14 @@
+const commands = {
+    addNewMonitoringScene: 'addNewMonitoringScene',
+    removeMonitoringScene: 'removeMonitoringScene',
+
+    addNewMonitoringAction: 'addNewMonitoring',
+    removeMonitoringAction: 'removeMonitoring',
+    removeAllMonitoringsAction: 'removeAllMonitorings',
+    showMonitoringsAction: 'showMonitorings',
+    runSearchAction: 'runSearch',
+
+    controls: 'controls',
+};
+
+module.exports = commands;

b63bd76f2f550fe838a37559cfb3eb8157d5b8ed
Roman Diachenko <dyaroman@gmail.com>
2019-08-24 10:55:49 +0300

    refactoring

--- modules/Commands.js
+++ modules/Commands.js
@@ -2,13 +2,12 @@ const commands = {
     addNewMonitoringScene: 'addNewMonitoringScene',
     removeMonitoringScene: 'removeMonitoringScene',
 
+    controls: 'controls',
     addNewMonitoring: 'add',
     removeMonitoring: 'remove',
     removeAllMonitorings: 'remove_all',
     showMonitorings: 'show',
     runSearch: 'search',
-
-    controls: 'controls',
 };
 
 module.exports = commands;

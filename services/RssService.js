017be37434f6953767f98da4931c5eb2251b7c84
Roman Diachenko <dyaroman@gmail.com>
2019-09-03 15:31:02 +0300

    update source array

--- services/RssService.js
+++ services/RssService.js
@@ -12,14 +12,22 @@ class RssService {
     init() {
         this.searchResults = [];
         this.sourcesArray = [
-            'http://feed.rutracker.cc/atom/f/4.atom', // Мультфильмы
+            'http://feed.rutracker.cc/atom/f/2343.atom', // Отечественные мультфильмы (HD Video)
+            'http://feed.rutracker.cc/atom/f/930.atom', // Иностранные мультфильмы (HD Video)
+            'http://feed.rutracker.cc/atom/f/2365.atom', // Иностранные короткометражные мультфильмы (HD Video)
+            'http://feed.rutracker.cc/atom/f/208.atom', // Отечественные мультфильмы
+            'http://feed.rutracker.cc/atom/f/539.atom', // Отечественные полнометражные мультфильмы
+            'http://feed.rutracker.cc/atom/f/209.atom', // Иностранные мультфильмы
+            'http://feed.rutracker.cc/atom/f/484.atom', // Иностранные короткометражные мультфильмы
+
+            'http://feed.rutracker.cc/atom/f/1460.atom', // Мультсериалы (HD Video)
+
             'http://feed.rutracker.cc/atom/f/7.atom', // Зарубежное кино
+            'http://feed.rutracker.cc/atom/f/1950.atom', // Фильмы 2019
+
             'http://feed.rutracker.cc/atom/f/33.atom', // Аниме
             'http://feed.rutracker.cc/atom/f/189.atom', // Зарубежные сериалы
-            'http://feed.rutracker.cc/atom/f/209.atom', // Иностранные мультфильмы
             'http://feed.rutracker.cc/atom/f/313.atom', // Зарубежное кино (HD Video)
-            'http://feed.rutracker.cc/atom/f/930.atom', // Иностранные мультфильмы (HD Video)
-            'http://feed.rutracker.cc/atom/f/1950.atom', // Фильмы 2019
             'http://feed.rutracker.cc/atom/f/2198.atom', // HD Video
             'http://feed.rutracker.cc/atom/f/2366.atom', // Зарубежные сериалы (HD Video)
         ];

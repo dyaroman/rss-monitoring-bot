b5f062edd966146db14deb64cfe1ad0c69ff6f3e
Roman Diachenko <dyaroman@gmail.com>
2019-12-26 17:36:05 +0200

    update time format in LogService

--- src/services/LogService.mjs
+++ src/services/LogService.mjs
@@ -17,7 +17,7 @@ export class LogService {
                     history: [
                         {
                             action: 'start',
-                            time: new Date().toUTCString(),
+                            time: new Date().toLocaleString(),
                         },
                     ],
                 },
@@ -30,7 +30,7 @@ export class LogService {
         this.db.collection('logs').updateOne({_id: userId}, {
             $push: {
                 history: Object.assign(obj, {
-                    time: new Date().toUTCString(),
+                    time: new Date().toLocaleString(),
                 }),
             },
         });

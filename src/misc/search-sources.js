ee6bbf4a0de15fd6eb6dd71b61602356c369c03f
Roman Diachenko <dyaroman@gmail.com>
2020-07-23 11:04:19 +0300

    add more search sources

--- src/misc/search-sources.js
+++ src/misc/search-sources.js
@@ -3,15 +3,23 @@ module.exports = [
     'http://feed.rutracker.cc/atom/f/33.atom',    // Аниме
     'http://feed.rutracker.cc/atom/f/124.atom',   // Арт-хаус и авторское кино
     'http://feed.rutracker.cc/atom/f/189.atom',   // Зарубежные сериалы
+    'http://feed.rutracker.cc/atom/f/193.atom',   // Сериалы Великобритании и Ирландии (HD Video)
     'http://feed.rutracker.cc/atom/f/208.atom',   // Отечественные мультфильмы
     'http://feed.rutracker.cc/atom/f/209.atom',   // Иностранные мультфильмы
+    'http://feed.rutracker.cc/atom/f/235.atom',   // Сериалы США и Канады
+    'http://feed.rutracker.cc/atom/f/236.atom',   // Звёздный путь / Star Trek
+    'http://feed.rutracker.cc/atom/f/242.atom',   // Сериалы Великобритании и Ирландии
+    'http://feed.rutracker.cc/atom/f/266.atom',   // Сериалы США и Канады (HD Video)
     'http://feed.rutracker.cc/atom/f/312.atom',   // Наше кино (HD Video)
     'http://feed.rutracker.cc/atom/f/313.atom',   // Зарубежное кино (HD Video)
     'http://feed.rutracker.cc/atom/f/484.atom',   // Иностранные короткометражные мультфильмы
     'http://feed.rutracker.cc/atom/f/539.atom',   // Отечественные полнометражные мультфильмы
     'http://feed.rutracker.cc/atom/f/893.atom',   // Японские мультфильмы
     'http://feed.rutracker.cc/atom/f/930.atom',   // Иностранные мультфильмы (HD Video)
+    'http://feed.rutracker.cc/atom/f/1102.atom',  // Европейские сериалы
     'http://feed.rutracker.cc/atom/f/1105.atom',  // Аниме (HD Video)
+    'http://feed.rutracker.cc/atom/f/1457.atom',  // UHD Video
+    'http://feed.rutracker.cc/atom/f/1459.atom',  // Европейские сериалы (HD Video)
     'http://feed.rutracker.cc/atom/f/1460.atom',  // Мультсериалы (HD Video)
     'http://feed.rutracker.cc/atom/f/1950.atom',  // Фильмы 2020
     'http://feed.rutracker.cc/atom/f/2198.atom',  // HD Video
@@ -19,4 +27,5 @@ module.exports = [
     'http://feed.rutracker.cc/atom/f/2343.atom',  // Отечественные мультфильмы (HD Video)
     'http://feed.rutracker.cc/atom/f/2365.atom',  // Иностранные короткометражные мультфильмы (HD Video)
     'http://feed.rutracker.cc/atom/f/2366.atom',  // Зарубежные сериалы (HD Video)
+    'http://feed.rutracker.cc/atom/f/2395.atom',  // Звёздный путь / Star Trek (HD Video)
 ];
